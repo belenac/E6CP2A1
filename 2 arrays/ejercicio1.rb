779ba1867ed01b133b2cd17b784f4c7d249bcbdd
belenac <33696123+belenac@users.noreply.github.com>
2018-04-28 00:01:31 -0300

    Ejercicio 1 Arrays

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -1,8 +1,21 @@
 # Dado el array:
 # 1. Mostrar en pantalla el primer elemento.
-# 2. Mostrar en pantalla el último elemento.
+# 2. Mostrar en pantalla el ultimo elemento.
 # 3. Mostrar en pantalla todos los elementos.
-# 4. Mostrar en pantalla todos los elementos junto con un índice.
-# 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
+# 4. Mostrar en pantalla todos los elementos junto con un indice.
+# 5. Mostrar en pantalla todos los elementos que se encuentren en una posicion pares
 
-arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+arreglo = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+puts arreglo.first
+puts arreglo.last
+print arreglo
+puts
+print arreglo[2]
+print arreglo[4]
+print arreglo[6]
+print arreglo[8]
+print arreglo[10]
+
+# puts arreglo.select { |num|  num.even? } seleccionar los pares
+# puts arreglo.sort ordenar los elementos
