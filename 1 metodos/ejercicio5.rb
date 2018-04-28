a152359db5e7dc7aa35d7bd0c6079a54285c5c34
belenac <33696123+belenac@users.noreply.github.com>
2018-04-27 23:41:08 -0300

    Ejercicio 5 Metodos

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,15 @@
-# Crear un método que reciba como parámetro dos números enteros positivos
-# e imprima los números pares que existen entre esos dos números.
+# Crear un metodo que reciba como parametro dos numeros enteros positivos
+# e imprima los numeros pares que existen entre esos dos numeros.
+# .even?
+
+def num_enteros(num1, num2)
+  if (num1 % 2).zero? && (num2 % 2).zero?
+    puts "#{num1} y #{num2} Par"
+  elsif (num1 % 2).zero?
+    puts "#{num1} Par"
+  elsif (num2 % 2).zero?
+    puts "#{num2} Par"
+  end
+end
+
+num_enteros(1, 6)
