c4f706c8aa89be64f5e314bac255a584855059de
belenac <33696123+belenac@users.noreply.github.com>
2018-04-27 23:40:39 -0300

    Ejercicio 4 Metodos

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,15 @@
-# Crear un método que muestre en pantalla un saludo, el método
-# debe recibir un parámetro, si ese parámetro es el string "Hola" el
-# método debe mostrar en pantalla "Hola Mundo".
+# Crear un metodo que muestre en pantalla un saludo, el metodo
+# debe recibir un parametro, si ese parametro es el string "Hola" el
+# metodo debe mostrar en pantalla "Hola Mundo".
+
+def saludo(sld)
+  if sld == 'Hola'
+    puts 'Hola Mundo'
+  else
+    puts 'Buenos días'
+  end
+end
+
+puts 'Escriba un saludo ej: Hola'
+saludar = gets.chomp!
+saludo(saludar)
